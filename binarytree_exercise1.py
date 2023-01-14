71e6838fdbde5c9f8f8639aec45b696a862cd883
unknown <santosbasty4@gmail.com>
2023-01-14 10:24:37 +0800

    First Commit - Base

---
+++ binarytree_exercise1.py
@@ -0,0 +1,66 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+    
+    def in_order_traversal(self):
+        elements = []
+
+        #visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        # visit base mode
+        elements.append(self.data)
+
+        # visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+        
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    sebastian = ["s", "e", "b", "a", "s", "t", "i", "a", "n", "o", "r", "d", "o", "n", "i", "o", "s", "a", "n", "t", "o", "s"]
+    name_tree = build_tree(sebastian)
+    print(name_tree.in_order_traversal())
